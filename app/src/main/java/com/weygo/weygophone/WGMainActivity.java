eb276c5f2578715e1e05f5e0bc62d231630800da
zhengyuanqian <zhengyuanqian@lifang.com>
2016-12-13 16:12:52 +0800

    创建项目

---
+++ app/src/main/java/com/weygo/weygophone/WGMainActivity.java
@@ -0,0 +1,13 @@
+package com.weygo.weygophone;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WGMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wgmain);
+    }
+}
