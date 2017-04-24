e0883013128f5ce47189998929b4f0455e434eb4
muma <zhengyuanqian@lifang.com>
2017-04-24 12:05:35 +0800

    侧边栏

--- app/src/main/java/com/weygo/weygophone/pages/slider/WGSliderActivity.java
+++ app/src/main/java/com/weygo/weygophone/pages/slider/WGSliderActivity.java
@@ -1,8 +1,13 @@
 package com.weygo.weygophone.pages.slider;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Looper;
+import android.util.Log;
+import android.view.View;
 
 import com.weygo.weygophone.R;
+import com.weygo.weygophone.WGMainActivity;
 import com.weygo.weygophone.base.WGBaseActivity;
 
 /**
@@ -13,11 +18,27 @@ public class WGSliderActivity extends WGBaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        overridePendingTransition(R.anim.trans_left_in, R.anim.trans_main);
+        setContentView(R.layout.wg_slider_main);
+        overridePendingTransition(R.anim.trans_right_in, R.anim.trans_main);
+        Log.e("asdfasdfad", "asdfasdfasd");
+        View blackView = (View) findViewById(R.id.slider_blankView);
+        blackView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                overridePendingTransition(R.anim.trans_main, R.anim.trans_left_out);
+                Intent intent = new Intent(WGSliderActivity.this, WGMainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     public void onBackPressed() {
         super.onBackPressed();
+        this.handleHiddenActivity();
+    }
+
+    private void handleHiddenActivity() {
         overridePendingTransition(R.anim.trans_main, R.anim.trans_left_out);
     }
 }
